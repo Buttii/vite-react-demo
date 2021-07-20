e45d99fd9eadd55256494e912f2e2ff108c2670c
chen_bd <cbd35134@163.com>
2021-07-20 19:20:37 +0800

    chore:修改前端项目启动端口号

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
     server: {
-        port: 4000,
+        port: 8000,
         open: true,
         proxy: {
             "^/api/.*": {
